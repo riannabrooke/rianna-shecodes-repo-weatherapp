71fba7536dcf125a44e0914bbece12de3eb186b3
riannabrooke <riannawashington@gmail.com>
2024-03-13 08:58:18 +0100

    created new functions that calls the api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
+function getCityData(city) {
+     let apiKey = "7d4d0f8bca21ta4eb4313oad2f2b1ec4";
+     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
+     axios.get(apiUrl).then(refreshWeather);
+}
+
+
+
 function handleSearch(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    getCityData(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
